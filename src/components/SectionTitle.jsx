bece2b55be1bf2e6aad2d25801282091b8470358
Alex Batistta <batistta.alex@outlook.com>
2025-05-03 10:18:05 -0300

    line on section title

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -5,6 +5,11 @@ import { capitalize } from '../utils';
 export const SectionTitle = ({ section }) => {
 	const { t } = useTranslation();
 	return (
-		<h2 className="subtitle">{capitalize(t(`navbar.${section}`))}</h2>
+		<div className='flex w-full items-center'>
+			<div className='w-[2%] bg-secondary h-[2px] mr-4 mt-1' />
+			<h2 className="subtitle whitespace-nowrap">{capitalize(t(`navbar.${section}`))}</h2>
+			<div className='w-full bg-secondary h-[2px] ml-4 mt-1' />
+		</div>
+
 	)
 }
